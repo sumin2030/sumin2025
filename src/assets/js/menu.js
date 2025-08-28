b8cd162f5c170e722fecc6ac1631424f5f8e9773
sumin2030 <dltnals2030@naver.com>
2025-08-28 16:00:51 +0900

    08.28

--- src/assets/js/menu.js
+++ src/assets/js/menu.js
@@ -1,3 +1,23 @@
 export function menu() {
-    console.log(menu)
+  
+    const headerToggle = document.getElementById("headerToggle");
+    const headerNav = document.querySelector(".header__nav");
+
+    if (headerToggle) {
+        console.log("헤더토굴!")
+
+        headerToggle.addEventListener("click", () => {
+
+
+            console.log("누군가 클릭함!!")
+
+
+
+            headerNav.classList.toggle("show");
+            headerToggle.getAttribute("aria-expanded") === "true"
+                ? headerToggle.setAttribute("aria-expanded", "false")
+                : headerToggle.setAttribute("aria-expanded", "true");
+        });
+    }
+
 }

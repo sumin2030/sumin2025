8158c43900b2a1572926ca542807476db22c88fc
sumin2030 <dltnals2030@naver.com>
2025-10-08 15:40:06 +0900

    2025.10.08 port 해결..?

--- src/assets/js/port.js
+++ src/assets/js/port.js
@@ -6,12 +6,13 @@ export function port() {
     const horSection = gsap.utils.toArray(".port__item");
  
     gsap.to(horSection, {
-        xPercent: -120 * (horSection.length - 1),
+        xPercent: -120 * (horSection.length - 4),
         ease: "none",
         scrollTrigger: {
             trigger: "#port",
             start: "top 56px",
-            end: "+=3000",
+           // end: "+=3000",
+            end: ()=> "+=" + document.querySelector("#port_wrap_area").offsetWidth,
             pin: true,
             scrub: 1,
             markers: false,
